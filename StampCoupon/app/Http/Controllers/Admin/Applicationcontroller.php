37905a43e0987180723d10c1cb83881da1c81ad8
TrongHiep2001 <hiepqt@paditech.com>
2022-09-23 11:23:37 +0700

    fix application controller create,update and write ApplicationRequest

--- StampCoupon/app/Http/Controllers/Admin/Applicationcontroller.php
+++ StampCoupon/app/Http/Controllers/Admin/Applicationcontroller.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Application;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
+use App\Http\Requests\ApplicationRequest;
 
 class ApplicationController extends Controller
 {
@@ -28,17 +29,13 @@ class ApplicationController extends Controller
     }
 
 
-    public function store(Request $request)
+    public function store(ApplicationRequest $request)
     {
+        $request->validated();
+        $params = $request->all();
+
         $app = new Application();
-        $app->app_name = $request->input('app_name');
-        if(!empty($request->logo)){
-            $generatedImage = 'image'.time().'-'. $request->app_name .'.'.$request->logo->extension();
-            //move to a folder
-            $request->logo->move(public_path('images'),$generatedImage);
-            $app->logo = $generatedImage;
-        }
-        $app->save();
+        $app->create($params);
         return redirect('admin/application');
 ;    }
 
@@ -55,25 +52,30 @@ class ApplicationController extends Controller
 
     public function update(Request $request, $id)
     {
-        $app = Application::find($id);
-        if(!empty($request->logo)){
-            $destination = 'images/'.$app->logo;
-            // dd($destination);
-            if(File::exists($destination)){
-                File::delete($destination);
-            };
-            $generatedImage = 'image'.time().'-'. $request->app_name .'.'.$request->logo->extension();
-            //move to a folder
-            $request->logo->move(public_path('images'),$generatedImage);
-            $app->update([
-                'app_name' => $request->input('app_name'),
-                'logo' => $generatedImage
-            ]);
-        }
-        else{
-            $app->update(['app_name' => $request->input('app_name') ]);
-        }
+        
+        $app = new Application();
+        $params = $request->all();
+        $app->updateApp($params,$id);
         return redirect('admin/application')->with('success','Update success!');
+        
+        // if(!empty($request->logo)){
+        //     $destination = 'images/'.$app->logo;
+        //     // dd($destination);
+        //     if(File::exists($destination)){
+        //         File::delete($destination);
+        //     };
+        //     $generatedImage = 'image'.time().'-'. $request->app_name .'.'.$request->logo->extension();
+        //     //move to a folder
+        //     $request->logo->move(public_path('images'),$generatedImage);
+        //     $app->update([
+        //         'app_name' => $request->input('app_name'),
+        //         'logo' => $generatedImage
+        //     ]);
+        // }
+        // else{
+        //     $app->update(['app_name' => $request->input('app_name') ]);
+        // }
+        // return redirect('admin/application')->with('success','Update success!');
     }
 
     public function destroy($id)
