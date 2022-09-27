ab0fee1464957c465bb408e0fe051bcbcb81bf62
TrongHiep2001 <hiepqt@paditech.com>
2022-09-27 15:24:42 +0700

    change function checkLogin

--- StampCoupon/app/Http/Controllers/Admin/LoginController.php
+++ StampCoupon/app/Http/Controllers/Admin/LoginController.php
@@ -9,22 +9,8 @@ use Illuminate\Support\Facades\Session;
 
 class LoginController extends Controller
 {
-    public function check_login(Request $request)
+    public function checkLogin(Request $request)
     {
-//         $remember = (bool)$request->remember;
-//         if (Auth::guard('admin')->attempt(['email' => $request->email,
-//             'password' => $request->password,
-//             'type' => 1,
-// //            $remember,
-//         ], $remember)) {
-//             return redirect()->route('admin.home');
-//         }
-//         if (Auth::guard('admin')->attempt(['email' => $request->email,
-//             'password' => $request->password,
-//             'type' => 0,
-//         ], $remember)) {
-//             return redirect()->route('admin.home');
-//         }
         if (Auth::guard('admin')->attempt(['email' => $request->email,
         'password' => $request->password,
         ])) {
@@ -35,9 +21,6 @@ class LoginController extends Controller
     }
 
     public function login(){
-        // if (auth()->check()) {
-        //     return redirect('/');
-        // }
         return view('admin.login.login');
     }
 
